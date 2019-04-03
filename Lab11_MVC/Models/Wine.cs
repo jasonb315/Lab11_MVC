5c8125cac166f1766f9f32397a99104e938a85ef
Jason Burns <jasonb315@protonmail.com>
2019-04-02 19:49:11 -0700

    wine method notes

--- Lab11_MVC/Models/Wine.cs
+++ Lab11_MVC/Models/Wine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,6 +21,24 @@ namespace Lab11_MVC.Models
 
         public static List<Wine> GetWineList(int price, int points)
         {
+            List<Wine> wineSelection = new List<Wine>();
+
+            //var CSVreader = new StreamReader(File.OpenRead("../../wine.csv"));
+
+            //string path = "../../wine.csv";
+            //var data = "";
+
+            //using (StreamReader sr = File.OpenText(path))
+            //{
+            //    data = sr.ReadToEnd();
+            //}
+
+            // bring in data
+            // read line by line, exclude header
+            // split lines into data sections array
+            // use data sections of arr by index to populate wine class constructor fields
+            // populate winSelection with each instantiated line
+            // return populated list to View calling wine.GerWineList
 
         }
 
